5aaf5fc9e9a3672c37e81837a3558ed0bfb3cdbe
Levente Kurusa <lev@boilerplate.co>
2025-06-23 06:09:54 +0200

    force dev mode

--- src/test/java/com/advancedraidtracker/AdvancedRaidTrackerPluginTest.java
+++ src/test/java/com/advancedraidtracker/AdvancedRaidTrackerPluginTest.java
@@ -3,11 +3,16 @@ package com.advancedraidtracker;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
+import java.util.Arrays;
+import java.util.Arrays;
+
 public class AdvancedRaidTrackerPluginTest
 {
     public static void main(String[] args) throws Exception
     {
         ExternalPluginManager.loadBuiltin(AdvancedRaidTrackerPlugin.class);
-        RuneLite.main(args);
+        String[] debugArgs = Arrays.copyOf(args, args.length + 1);
+        debugArgs[args.length] = "--developer-mode";
+        RuneLite.main(debugArgs);
     }
 }
